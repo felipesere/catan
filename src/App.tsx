60dc37dc6c558266d9fa48776e866276717bb66e
Felipe Sere <felipesere@gmail.com>
2020-04-19 14:37:06 +0200

    Mostly working tiles with SVG clipping path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import './App.css';
 import {Board} from "./components/board/board";
+import {Terrain} from "./types";
+
+const random = (): Terrain => {
+    let possible = ['forest', 'pasture', 'hill', 'mountain', 'field', 'desert'];
+
+    return possible[Math.floor(Math.random() * possible.length)] as Terrain;
+}
+
+const range = (amount: number): number[] => {
+    return Array.from(Array(amount).keys())
+}
 
 function App() {
     return (
@@ -8,9 +19,13 @@ function App() {
             <header className="App-header">
                 Welcome to Catan
             </header>
-            <Board hexList={[]} roadList={[]} townList={[]} />
+            <Board hexList={range(19).map((i) => ({
+                terrain: random(),
+                dieRoll: i,
+            }))}
+                   roadList={[]} townList={[]}/>
         </div>
-);
+    );
 }
 
 export default App;
